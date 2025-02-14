9400d41d5e52aea5da4a236411a63e7c07560d84
Ankit-Mohanta <mohantaankit2002@gmail.com>
2025-02-14 13:08:30 +0530

    Database code added

--- full-stack-backend/config/mongodb.js
+++ full-stack-backend/config/mongodb.js
@@ -1,9 +1,14 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-
-    mongoose.connection.on('connected', () => console.log("Database Connected"))
-    await mongoose.connect(`${process.env.MONGODB_URI}/ankit`)
+    try {
+        mongoose.connection.on('connected', () => console.log("Database Connected"))
+        await mongoose.connect(`${process.env.MONGODB_URI}/ankit`).then(() => {
+            console.log("Database connection successful")
+        })
+    } catch (error) {
+        console.error(error)
+    }
 
 }
 
--- full-stack-backend/server.js
+++ full-stack-backend/server.js
@@ -11,7 +11,7 @@ import adminRouter from "./routes/adminRoute.js"
 // app config
 const app = express()
 const port = process.env.PORT || 4000
-connectDB()
+await connectDB()
 connectCloudinary()
 
 // middlewares
